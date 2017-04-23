8a708c2dd5fb4480b657af6e5e5fbf624c52e648
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-04-23 20:47:53 +0600

    testing deployment

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Server is under construction</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          It will be stable soon. Powered by <code>ReactJS</code>
         </p>
       </div>
     );
